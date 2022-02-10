a37e7f430d497b0ea4f14991986e88e660e7776d
Wei.Wang <1849553578@qq.com>
2022-02-11 07:50:55 +0800

    feat>JVM>GC

--- 005.OpenJDK/001.openJdk8-b120/jdk-jdk8-b120/hotspot/src/cpu/x86/vm/frame_x86.inline.hpp
+++ 005.OpenJDK/001.openJdk8-b120/jdk-jdk8-b120/hotspot/src/cpu/x86/vm/frame_x86.inline.hpp
@@ -31,13 +31,16 @@
 
 // Constructors:
 
+/**
+ * 栈帧结构
+ */ 
 inline frame::frame() {
-  _pc = NULL;
-  _sp = NULL;
-  _unextended_sp = NULL;
-  _fp = NULL;
-  _cb = NULL;
-  _deopt_state = unknown;
+  _pc = NULL;                             // 程序计数器，指向下一个要执行的代码地址
+  _sp = NULL;                             // 栈顶指针
+  _unextended_sp = NULL;                  // 异常栈顶指针
+  _fp = NULL;                             // 栈底指针
+  _cb = NULL;                             // 代码块地址
+  _deopt_state = unknown;                 // 描述从编译代码到解释代码的反优化的状态。
 }
 
 inline frame::frame(intptr_t* sp, intptr_t* fp, address pc) {
