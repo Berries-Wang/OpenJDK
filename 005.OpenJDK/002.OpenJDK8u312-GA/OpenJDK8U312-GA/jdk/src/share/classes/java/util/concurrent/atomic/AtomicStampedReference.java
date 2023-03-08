0784ed7ff1cfa0d16feea751e15ffb1e746acd39
Wei <1849553578@qq.com>
2023-03-08 08:07:21 +0800

    feat>JDK>CAS ABA问题解决之道-AtomicStampedReference

--- 005.OpenJDK/002.OpenJDK8u312-GA/OpenJDK8U312-GA/jdk/src/share/classes/java/util/concurrent/atomic/AtomicStampedReference.java
+++ 005.OpenJDK/002.OpenJDK8u312-GA/OpenJDK8U312-GA/jdk/src/share/classes/java/util/concurrent/atomic/AtomicStampedReference.java
@@ -39,23 +39,27 @@ package java.util.concurrent.atomic;
  * An {@code AtomicStampedReference} maintains an object reference
  * along with an integer "stamp", that can be updated atomically.
  *
+ * <p> AtomicStampedReference 维护一个对象引用和一个整数类型的"stamp",他们可以被原子性地更新。
+ *
  * <p>Implementation note: This implementation maintains stamped
  * references by creating internal objects representing "boxed"
  * [reference, integer] pairs.
  *
- * @since 1.5
- * @author Doug Lea
  * @param <V> The type of object referred to by this reference
+ * @author Doug Lea
+ * @since 1.5
  */
 public class AtomicStampedReference<V> {
 
     private static class Pair<T> {
         final T reference;
         final int stamp;
+
         private Pair(T reference, int stamp) {
             this.reference = reference;
             this.stamp = stamp;
         }
+
         static <T> Pair<T> of(T reference, int stamp) {
             return new Pair<T>(reference, stamp);
         }
@@ -67,7 +71,7 @@ public class AtomicStampedReference<V> {
      * Creates a new {@code AtomicStampedReference} with the given
      * initial values.
      *
-     * @param initialRef the initial reference
+     * @param initialRef   the initial reference
      * @param initialStamp the initial stamp
      */
     public AtomicStampedReference(V initialRef, int initialStamp) {
@@ -97,7 +101,7 @@ public class AtomicStampedReference<V> {
      * Typical usage is {@code int[1] holder; ref = v.get(holder); }.
      *
      * @param stampHolder an array of size of at least one.  On return,
-     * {@code stampholder[0]} will hold the value of the stamp.
+     *                    {@code stampholder[0]} will hold the value of the stamp.
      * @return the current value of the reference
      */
     public V get(int[] stampHolder) {
@@ -113,53 +117,54 @@ public class AtomicStampedReference<V> {
      * and the current stamp is equal to the expected stamp.
      *
      * <p><a href="package-summary.html#weakCompareAndSet">May fail
-     * spuriously and does not provide ordering guarantees</a>, so is
-     * only rarely an appropriate alternative to {@code compareAndSet}.
+     * spuriously(伪造地;虚假地;) and does not provide ordering guarantees(保证)</a>, so is
+     * only rarely(很少;罕有;) an appropriate(合适地;) alternative(替代物;) to {@code compareAndSet}.
+     * <p>weakCompareAndSet或许会虚假地失败且不提供顺序保证，所以，weakCompareAndSet很少有机会代替compareAndSet
      *
      * @param expectedReference the expected value of the reference
-     * @param newReference the new value for the reference
-     * @param expectedStamp the expected value of the stamp
-     * @param newStamp the new value for the stamp
+     * @param newReference      the new value for the reference
+     * @param expectedStamp     the expected value of the stamp
+     * @param newStamp          the new value for the stamp
      * @return {@code true} if successful
      */
-    public boolean weakCompareAndSet(V   expectedReference,
-                                     V   newReference,
+    public boolean weakCompareAndSet(V expectedReference,
+                                     V newReference,
                                      int expectedStamp,
                                      int newStamp) {
         return compareAndSet(expectedReference, newReference,
-                             expectedStamp, newStamp);
+                expectedStamp, newStamp);
     }
 
     /**
-     * Atomically sets the value of both the reference and stamp
+     * Atomically(以原子方式,原子的) sets the value of both the reference and stamp
      * to the given update values if the
      * current reference is {@code ==} to the expected reference
      * and the current stamp is equal to the expected stamp.
+     * <p>原子性地设置reference和stamp的值，在current.reference的值==expectedReference 且 current.stamp == expectedStamp
      *
      * @param expectedReference the expected value of the reference
-     * @param newReference the new value for the reference
-     * @param expectedStamp the expected value of the stamp
-     * @param newStamp the new value for the stamp
+     * @param newReference      the new value for the reference
+     * @param expectedStamp     the expected value of the stamp
+     * @param newStamp          the new value for the stamp
      * @return {@code true} if successful
      */
-    public boolean compareAndSet(V   expectedReference,
-                                 V   newReference,
+    public boolean compareAndSet(V expectedReference,
+                                 V newReference,
                                  int expectedStamp,
                                  int newStamp) {
         Pair<V> current = pair;
         return
-            expectedReference == current.reference &&
-            expectedStamp == current.stamp &&
-            ((newReference == current.reference &&
-              newStamp == current.stamp) ||
-             casPair(current, Pair.of(newReference, newStamp)));
+                expectedReference == current.reference
+                        && expectedStamp == current.stamp
+                        && ((newReference == current.reference && newStamp == current.stamp) || casPair(current, Pair.of(newReference, newStamp)));
     }
 
     /**
-     * Unconditionally sets the value of both the reference and stamp.
+     * Unconditionally(无条件地) sets the value of both the reference and stamp.
+     * <p>无条件地设置引用和戳的值
      *
      * @param newReference the new value for the reference
-     * @param newStamp the new value for the stamp
+     * @param newStamp     the new value for the stamp
      */
     public void set(V newReference, int newStamp) {
         Pair<V> current = pair;
@@ -168,31 +173,34 @@ public class AtomicStampedReference<V> {
     }
 
     /**
-     * Atomically sets the value of the stamp to the given update value
+     * Atomically(原子的) sets the value of the stamp to the given update value
      * if the current reference is {@code ==} to the expected
      * reference.  Any given invocation of this operation may fail
-     * (return {@code false}) spuriously, but repeated invocation
+     * (return {@code false}) spuriously(伪造地;虚假地;), but repeated invocation
      * when the current value holds the expected value and no other
-     * thread is also attempting to set the value will eventually
+     * thread is also attempting to set the value will eventually(最终)
      * succeed.
      *
+     * <p>当current.reference == expectedReference时，将stamp的值原子性的更新为指定的值。此方法任何给定调用都可能失败，返回false,
+     * 但若current.reference == expectedReference 且没有其他线程也尝试修改该值时，重复调用最终将成功。
+     *
      * @param expectedReference the expected value of the reference
-     * @param newStamp the new value for the stamp
+     * @param newStamp          the new value for the stamp
      * @return {@code true} if successful
      */
     public boolean attemptStamp(V expectedReference, int newStamp) {
         Pair<V> current = pair;
         return
-            expectedReference == current.reference &&
-            (newStamp == current.stamp ||
-             casPair(current, Pair.of(expectedReference, newStamp)));
+                expectedReference == current.reference &&
+                        (newStamp == current.stamp ||
+                                casPair(current, Pair.of(expectedReference, newStamp)));
     }
 
-    // Unsafe mechanics
+    // Unsafe mechanics(n. 机械学，力学；机制，运作方式；机械部件，运转部件；结构，构成)
 
     private static final sun.misc.Unsafe UNSAFE = sun.misc.Unsafe.getUnsafe();
     private static final long pairOffset =
-        objectFieldOffset(UNSAFE, "pair", AtomicStampedReference.class);
+            objectFieldOffset(UNSAFE, "pair", AtomicStampedReference.class);
 
     private boolean casPair(Pair<V> cmp, Pair<V> val) {
         return UNSAFE.compareAndSwapObject(this, pairOffset, cmp, val);
