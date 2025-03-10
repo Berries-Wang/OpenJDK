3ca434f65ee1bced1d686b0276c5add028f11a39
Wei.Wang <1849553578@qq.com>
2025-03-10 08:27:19 +0800

    OpenJDK JDK编译

--- 005.OpenJDK/000.OpenJDK_8U40/jdk/make/src/classes/build/tools/generatecurrencydata/GenerateCurrencyData.java
+++ 005.OpenJDK/000.OpenJDK_8U40/jdk/make/src/classes/build/tools/generatecurrencydata/GenerateCurrencyData.java
@@ -281,7 +281,7 @@ public class GenerateCurrencyData {
             checkCurrencyCode(newCurrency);
             String timeString = currencyInfo.substring(4, length - 4);
             long time = format.parse(timeString).getTime();
-            if (Math.abs(time - System.currentTimeMillis()) > ((long) 10) * 365 * 24 * 60 * 60 * 1000) {
+            if (Math.abs(time - System.currentTimeMillis()) > ((long) 100) * 365 * 24 * 60 * 60 * 1000) {
                 throw new RuntimeException("time is more than 10 years from present: " + time);
             }
             specialCaseCutOverTimes[specialCaseCount] = time;
